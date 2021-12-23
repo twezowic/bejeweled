d5c45a22fc645480b44656287632846a2c5ce32e
twezowic <01169258@pw.edu.pl>
2021-12-23 22:11:57 +0100

    Adjusted.

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ board_width = 20  # min 3
 board_height = 8  # min 3
 number_of_jewels = 5
 
+
 def choose_colors(number_of_colors):
     colors = []
     for i in range(1, number_of_colors+1):
@@ -12,5 +13,6 @@ def choose_colors(number_of_colors):
         r, g, b = [floor(255*i) for i in hls_to_rgb(h, l, s)]
         colors.append((r, g, b))
     return colors
+
 
 colors_of_jewels = choose_colors(number_of_jewels)
