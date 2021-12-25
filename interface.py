942376b12d6e211f40387cbafb61e1297ee23433
twezowic <01169258@pw.edu.pl>
2021-12-26 00:43:25 +0100

    Adjusted swap_jewels functionality. Replaced squares with circles.

--- interface.py
+++ interface.py
@@ -9,7 +9,10 @@ from config import (
 def interface(board):
     pygame.init()
     screen = pygame.display.set_mode((board_width*50, board_height*50))
-    jewel = pygame.Surface((50, 50))
+    background = pygame.Surface((board_width*50, board_height*50))
+    background.fill('white')
+    screen.blit(background, (0, 0))
+
     clock = pygame.time.Clock()
     pygame.display.set_caption('Bejeweled')
 
@@ -18,22 +21,42 @@ def interface(board):
     cursor_position = [25, 25]
     cursor_position_in_table = [0, 0]
 
-    select = False
     selected_position = []
+    select = False
 
     table = board.board()
 
     while True:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print('space')
+                    print(selected_position)
+                    print(cursor_position_in_table)
+                    if select is False:
+                        select = True
+                        selected_position = tuple(cursor_position_in_table)
+                        print(selected_position)
+                    elif selected_position != tuple(cursor_position_in_table):
+                        print('x')
+                        select = False
+                        board.swap_jewels((
+                            selected_position,
+                            cursor_position_in_table)
+                            )
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        for x in range(board_height):
-            for y in range(board_width):
-                jewel.fill(table[x][y].colour())
-                screen.blit(jewel, (x*50, y*50))
+        for y in range(board_height):
+            for x in range(board_width):
+                pygame.draw.ellipse((
+                    screen,
+                    table[y][x].colour(),
+                    pygame.Rect(x*50, y*50, 50, 50))
+                    )
 
+        pygame.key.set_repeat(0, 1000)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             if cursor_position[0] != board_width * 50 - 25:
@@ -52,19 +75,6 @@ def interface(board):
                 cursor_position_in_table[1] -= 1
                 cursor_position[1] -= 50
         screen.blit(cursor, (cursor_position))
-
-        if keys[pygame.K_SPACE]:
-            print('space')
-            print(selected_position)
-            print(cursor_position_in_table)
-            if select is False:
-                select = True
-                selected_position = tuple(cursor_position_in_table)
-                print(selected_position)
-            elif selected_position != cursor_position_in_table:
-                print('x')
-                select = False
-                board.swap_jewels(selected_position, cursor_position_in_table)
 
         pygame.display.update()
         clock.tick(30)
