c4a512719333a8ecc480042851ba903319ab1287
twezowic <01169258@pw.edu.pl>
2021-12-25 23:02:13 +0100

    Added Jewel class. Added swap_jewels method in Board class.

--- board.py
+++ board.py
@@ -3,6 +3,14 @@ from numpy import array
 from random import choice
 
 
+class Jewel:
+    def __init__(self, colour):
+        self._colour = colour
+
+    def colour(self):
+        return self._colour
+
+
 class Board:
     def __init__(self, board=None):
         if board is None:
@@ -15,12 +23,12 @@ class Board:
     def set_board(self, new_board):
         self._board = new_board
 
-    def create_board(self):  # x, y; ma generować tablicę bez usuwań
+    def create_board(self):
         board = []
-        for row in range(board_width):
+        for x in range(board_height):
             line = []
-            for jewel in range(board_height):
-                jewel = choice(colors_of_jewels)
+            for y in range(board_width):
+                jewel = Jewel(choice(colors_of_jewels))
                 line.append(jewel)
             board.append(line)
         board = array(board)
@@ -28,3 +36,15 @@ class Board:
 
     def destroy_jewels(self):
         pass
+
+    def show_board(self):
+        table = self.board()
+        for i in range(board_height):
+            for j in range(board_width):
+                print(table[i][j].colour())
+
+    def swap_jewels(self, position1, position2):
+        x1, y1 = position1
+        x2, y2 = position2
+        table = self.board()
+        table[x1][y1], table[x2][y2] = table[x2][y2], table[x1][y1]
