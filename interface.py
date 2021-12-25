a6553dc0f83898339222400b63f9d1f1f57962a7
twezowic <01169258@pw.edu.pl>
2021-12-25 23:01:15 +0100

    Added jewel swap functionality.

--- interface.py
+++ interface.py
@@ -15,33 +15,56 @@ def interface(board):
 
     cursor = pygame.Surface((5, 5))
     cursor.fill('white')
-    position = [25, 25]
+    cursor_position = [25, 25]
+    cursor_position_in_table = [0, 0]
+
+    select = False
+    selected_position = []
+
     table = board.board()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        for i in range(board_height):
-            for j in range(board_width):
-                jewel.fill(table[j][i])
-                screen.blit(jewel, (j*50, i*50))
+        for x in range(board_height):
+            for y in range(board_width):
+                jewel.fill(table[x][y].colour())
+                screen.blit(jewel, (x*50, y*50))
 
         keys = pygame.key.get_pressed()
-        if position[0] != board_width * 50 - 25:
-            position[0] += keys[pygame.K_RIGHT] * 50
-        if position[0] != 25:
-            position[0] -= keys[pygame.K_LEFT] * 50
-        if position[1] != board_height * 50 - 25:
-            position[1] += keys[pygame.K_DOWN] * 50
-        if position[1] != 25:
-            position[1] -= keys[pygame.K_UP] * 50
-        screen.blit(cursor, (position))
+        if keys[pygame.K_RIGHT]:
+            if cursor_position[0] != board_width * 50 - 25:
+                cursor_position_in_table[0] += 1
+                cursor_position[0] += 50
+        if keys[pygame.K_LEFT]:
+            if cursor_position[0] != 25:
+                cursor_position_in_table[0] -= 1
+                cursor_position[0] -= 50
+        if keys[pygame.K_DOWN]:
+            if cursor_position[1] != board_height * 50 - 25:
+                cursor_position_in_table[1] += 1
+                cursor_position[1] += 50
+        if keys[pygame.K_UP]:
+            if cursor_position[1] != 25:
+                cursor_position_in_table[1] -= 1
+                cursor_position[1] -= 50
+        screen.blit(cursor, (cursor_position))
+
+        if keys[pygame.K_SPACE]:
+            print('space')
+            print(selected_position)
+            print(cursor_position_in_table)
+            if select is False:
+                select = True
+                selected_position = tuple(cursor_position_in_table)
+                print(selected_position)
+            elif selected_position != cursor_position_in_table:
+                print('x')
+                select = False
+                board.swap_jewels(selected_position, cursor_position_in_table)
 
         pygame.display.update()
         clock.tick(30)
-
-
-if __name__ == '__main__':
-    interface()
