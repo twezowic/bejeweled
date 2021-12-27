b6bf55fbfa0807eb4c360ab5f4b76ed52a330aeb
twezowic <01169258@pw.edu.pl>
2021-12-27 20:41:03 +0100

    Created delete attribute and eq method. Created destroying_jewels method in Board class.

--- board.py
+++ board.py
@@ -1,18 +1,30 @@
 from config import board_width, board_height, colors_of_jewels
-from numpy import array
 from random import choice
+from numpy import array
 
 
 class Jewel:
-    def __init__(self, colour, special=False):
+    def __init__(self, colour, delete=False):
         self._colour = colour
-        self._special = special
+        self._delete = delete
 
     def colour(self):
         return self._colour
 
-    def special(self):
-        return self._special
+    def delete(self):
+        return self._delete
+
+    def set_delete(self, new_delete):
+        self._delete = new_delete
+
+    def set_colour(self, new_colour):
+        self._colour = new_colour
+
+    def __eq__(self, other) -> bool:
+        return self.colour() == other.colour()
+
+    def __str__(self) -> str:
+        return f'{self.colour()}'
 
 
 class Board:
@@ -38,9 +50,6 @@ class Board:
         board = array(board)
         self.set_board(board)
 
-    def destroy_jewels(self):
-        pass
-
     def show_board(self):
         table = self.board()
         for y in range(board_height):
@@ -54,3 +63,23 @@ class Board:
         x2, y2 = position2
         table = self.board()
         table[y1][x1], table[y2][x2] = table[y2][x2], table[y1][x1]
+
+    def destroying_jewels(self):
+        board = self.board()
+        for y in range(board_height):  # wiersze
+            for x in range(board_width - 2):
+                three = board[y, x:x+3]
+                if three[0] == three[1] == three[2]:
+                    for element in range(3):
+                        board[y][x+element].set_delete(True)
+        for x in range(board_width):  # kolumny
+            for y in range(board_height - 2):
+                three = board[y:y+3, x]
+                if three[0] == three[1] == three[2]:
+                    for element in range(3):
+                        board[y+element][x].set_delete(True)
+        for y in range(board_height):  # usuwanie
+            for x in range(board_width):
+                if board[y][x].delete():
+                    board[y][x].set_colour('white')
+                    board[y][x].set_delete(False)
