e00c4536c55e5f7f301947ed8018ee9070db03a1
twezowic <01169258@pw.edu.pl>
2021-12-27 22:46:47 +0100

    Added Player class.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import interface
 from board import Board
+from game import Player
 
 
 def main():
     board = Board()
+    player = Player()
     board.create_board()
-    interface.interface(board)
+    board.show_board()
+    interface.interface(board, player)
 
 
 if __name__ == '__main__':
