92934cb80ff6aff58e886fe895cce945853e311e
twezowic <01169258@pw.edu.pl>
2021-12-27 22:46:25 +0100

    Created game.py file. Implemented get_highscore and new_highscore function. Created Player class.

---
+++ game.py
@@ -0,0 +1,25 @@
+from config import number_of_moves
+
+
+def get_highscore():
+    with open('highscore.txt', 'r') as file_handle:
+        return int(file_handle.readline())
+
+
+def new_highscore(highscore):
+    with open('highscore.txt', 'w') as file_handle:
+        file_handle.writelines(f'{highscore}')
+
+
+class Player:
+    def __init__(self, moves=number_of_moves):
+        self._moves = moves
+
+    def moves(self):
+        return self._moves
+
+    def set_moves(self, new_moves):
+        self._moves = new_moves
+
+    def one_move(self):
+        self.set_moves(self.moves()-1)
