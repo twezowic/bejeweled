506b1bac2c0b0f4c3f4fa1de061617e939908b40
twezowic <01169258@pw.edu.pl>
2021-12-30 01:08:24 +0100

    Implemented new_functions.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import interface
+from interface import interface
 from board import Board
 from game import Player
 
@@ -7,8 +7,8 @@ def main():
     board = Board()
     player = Player()
     board.create_board()
-    board.show_board()
-    interface.interface(board, player)
+    board.setup_board()
+    interface(board, player)
 
 
 if __name__ == '__main__':
