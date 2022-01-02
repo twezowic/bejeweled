a314a97b31cc87885c32b0848b62f1aa3183029e
twezowic <01169258@pw.edu.pl>
2022-01-02 01:54:15 +0100

    Added json file support.

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from config import number_of_moves
+import json
 
 
 def get_highscore():
@@ -14,9 +15,70 @@ def new_highscore(highscore):
         file_handle.writelines(f'{highscore}')
 
 
-class Player:
-    def __init__(self, name=None, score=0, moves=number_of_moves):
+class Score:
+    def __init__(self, name, score):
         self._name = name
+        self._score = score
+
+    def name(self):
+        return self._name
+
+    def score(self):
+        return self._score
+
+
+class Leadeboard:
+    def __init__(self, scores):
+        self._scores = scores
+
+    def scores(self):
+        return self._scores
+
+    def set_scores(self, new_scores):
+        self._scores = new_scores
+
+    def get_from_json(self):
+        with open('leaderboard.json', 'r') as file_handle:
+            leadeboard = []
+            data = json.load(file_handle)
+            for element in data:
+                name = element['name']
+                score = element['score']
+                leadeboard.append(Score(name, score))
+        return Leadeboard(leadeboard)
+
+    def set_to_json(self):
+        with open('leaderboard.json', 'w') as file_handle:
+            data = []
+            for element in self.scores():
+                name = element.name()
+                score = element.score()
+                score_data = {
+                    'name': name,
+                    'score': score
+                }
+                data.append(score_data)
+            json.dump(data, file_handle, indent=4)
+
+    def __str__(self):
+        scores = self.scores()
+        result = ''
+        for index, score in enumerate(scores):
+            result += f'{index}. Player: {score.name()} {score.score()}\n'
+        return result
+
+    def adding_new_score(self, new_score):
+        leadeboard = self.scores()
+        new_leaderboard = []
+        for data in leadeboard:
+            if new_score.score() > data.score():
+                new_leaderboard.append(new_score)
+            new_leaderboard.append(data)
+        self.set_scores(new_leaderboard[0:9])
+
+
+class Game:
+    def __init__(self, score=0, moves=number_of_moves):
         self._moves = moves
         self._score = score
 
