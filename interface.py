616c035e2bae4b9c1d284a5c66e234d29b6dd417
twezowic <01169258@pw.edu.pl>
2022-01-03 00:21:49 +0100

    Added menu interface and implemented leaderboard class.

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 import pygame
 from sys import exit
-from game import get_highscore, new_highscore
+from game import Score
 from config import (
     board_height,
     board_width,
@@ -34,7 +34,7 @@ def moving(cursor_position):
         cursor_position[1] -= 1
 
 
-def interface(board, game):
+def interface(board, game, leaderboard):
     pygame.init()
     SCREEN_WIDTH = board_width*50+110
     SCREEN_HEIGHT = board_height*50+10
@@ -44,6 +44,7 @@ def interface(board, game):
 
     font = pygame.font.Font('OpenSans.ttf', 12)
     font_title = pygame.font.Font('OpenSans.ttf', 32)
+    font_menu = pygame.font.Font('Opensans.ttf', 16)
 
     jewel = pygame.image.load('jewel.png')
     jewel_rect = jewel.get_rect(center=(SCREEN_WIDTH/2, 128))
@@ -65,7 +66,46 @@ def interface(board, game):
         SCREEN_HEIGHT/2
         ))
 
+    # menu screen
     player_name = ''
+    name_info = font_menu.render(
+        'Please enter your name. Press space to confirm.',
+        True,
+        'Black'
+        )
+    name_info_rect = name_info.get_rect(center=(
+        SCREEN_WIDTH/2,
+        20
+        ))
+    mode_info = font_menu.render(
+        'Please choose mode of game',
+        True,
+        'Black'
+        )
+    mode_info_rect = mode_info.get_rect(center=(
+        SCREEN_WIDTH/2,
+        60
+        ))
+    normal_info = font_menu.render(
+        'normal',
+        True,
+        'Blue'
+        )
+    normal_info_rect = normal_info.get_rect(center=(
+        SCREEN_WIDTH/2,
+        80
+        ))
+    endless_info = font_menu.render(
+        'endless',
+        True,
+        'Blue'
+        )
+    endless_info_rect = endless_info.get_rect(center=(
+        SCREEN_WIDTH/2,
+        100
+        ))
+    mode = False
+    normal = True
 
     pygame.display.set_icon(jewel)
 
@@ -76,13 +116,13 @@ def interface(board, game):
     selected_position = []
     select = False
 
-    highscore = get_highscore()
+    highscore = leaderboard.get_highscore()
 
     table = board.board()
 
     title_screen = True
     menu = False
-    ending = False
+    active = False
 
     while True:
         for event in pygame.event.get():
@@ -92,16 +132,25 @@ def interface(board, game):
                         title_screen = False
                         menu = True
                 elif menu:
-                    if event.key == pygame.K_BACKSPACE:
-                        player_name = player_name[:-1]
-                    elif event.key == pygame.K_SPACE:
-                        menu = False
-                    else:
-                        player_name += event.unicode
-                elif ending:
+                    if not mode:
+                        if event.key == pygame.K_BACKSPACE:
+                            player_name = player_name[:-1]
+                        else:
+                            player_name += event.unicode
+                    if event.key == pygame.K_SPACE:
+                        if mode is False:
+                            mode = True
+                        else:
+                            mode = False
+                            menu = False
+                            active = True
+                    elif event.key == pygame.K_DOWN:
+                        normal = False
+                    elif event.key == pygame.K_UP:
+                        normal = True
+                elif not active:
                     if event.key == pygame.K_SPACE:
                         title_screen = True
-                        ending = False
                 else:
                     if event.key == pygame.K_SPACE:
                         # zamiana klejnotow
@@ -128,7 +177,6 @@ def interface(board, game):
             # zamykanie okna
             if event.type == pygame.QUIT:
                 pygame.quit()
-                new_highscore(highscore)
                 exit()
 
         screen.fill('bisque')
@@ -141,14 +189,36 @@ def interface(board, game):
             name_text = font.render(player_name, True, 'black')
             name_text_rect = name_text.get_rect(center=(
                 SCREEN_WIDTH/2,
-                SCREEN_HEIGHT/2
+                40
             ))
             screen.blit(name_text, name_text_rect)
+            screen.blit(name_info, name_info_rect)
+            if mode:
+                screen.blit(mode_info, mode_info_rect)
+                screen.blit(normal_info, normal_info_rect)
+                screen.blit(endless_info, endless_info_rect)
+                if normal:
+                    position = (
+                        SCREEN_WIDTH/2-40,
+                        70,
+                        80,
+                        20
+                    )
+                else:
+                    position = (
+                        SCREEN_WIDTH/2-40,
+                        90,
+                        80,
+                        20
+                    )
+                pygame.draw.rect(
+                    screen,
+                    'red',
+                    pygame.Rect(position),
+                    2
+                    )
 
-        elif ending:
-            screen.blit(ending_text, ending_text_rect)
-
-        elif game.moves() > 0:
+        elif active:
             # przygotowanie tekstu
             score = game.score()
             score_text = font.render(
@@ -175,8 +245,11 @@ def interface(board, game):
             moving(cursor_position)
 
             # automatyczne działania na planszy
-            if board.game_over():
-                ending = True
+            if board.game_over(game.moves()):
+                active = False
+                leaderboard.adding_new_score(Score(player_name, score))
+                leaderboard.set_to_json()
+                player_name = ''
             board.jewel_refill()
             game.set_score(score + board.destroying_jewels())
 
@@ -206,8 +279,9 @@ def interface(board, game):
                     pygame.Rect(x-20, y-20, 50, 50),
                     5
                     )
+
         else:
-            ending = True
+            screen.blit(ending_text, ending_text_rect)
 
         pygame.display.update()
         clock.tick(10)
