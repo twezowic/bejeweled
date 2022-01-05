f7cbed03da63a88f3fac09b9305ec7294e3fb5d8
twezowic <01169258@pw.edu.pl>
2022-01-05 01:29:24 +0100

    Added Screen width and height.

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ from math import floor
 
 board_width = 8  # min 8
 board_height = 8  # min 6
+SCREEN_WIDTH = board_width*50+110
+SCREEN_HEIGHT = board_height*50+10
 number_of_jewels = 6
-number_of_moves = 10
+number_of_moves = 3
 
 
 def choose_colors(number_of_colors):
