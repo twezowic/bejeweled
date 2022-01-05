ca5d83ab804a4c41f3534ff31394a44992b74a74
twezowic <01169258@pw.edu.pl>
2022-01-05 01:29:01 +0100

    Added mode, name and highscore method.

--- game.py
+++ game.py
@@ -60,7 +60,8 @@ class Leadeboard:
             result += f'{index}. Player: {score.name()} {score.score()}\n'
         return result
 
-    def adding_new_score(self, new_score):
+    def adding_new_score(self, player_name, score):
+        new_score = Score(player_name, score)
         leadeboard = self.scores()
         if not leadeboard:
             self.set_scores([new_score])
@@ -77,9 +78,14 @@ class Leadeboard:
 
 
 class Game:
-    def __init__(self, score=0, moves=number_of_moves):
+    def __init__(self, score=0, moves=number_of_moves, normal_mode=True, name=None, highscore=0):
         self._moves = moves
         self._score = score
+        self._normal_mode = normal_mode
+        if name is None:
+            name = ''
+        self._name = name
+        self._highscore = highscore
 
     def moves(self):
         return self._moves
@@ -95,3 +101,21 @@ class Game:
 
     def set_score(self, new_score):
         self._score = new_score
+
+    def normal_mode(self):
+        return self._normal_mode
+
+    def change_normal_mode(self):
+        self._normal_mode = not self.normal_mode()
+
+    def name(self):
+        return self._name
+
+    def set_name(self, new_name):
+        self._name = new_name
+
+    def highscore(self):
+        return self._highscore
+
+    def set_highscore(self, new_highscore):
+        self._highscore = new_highscore
