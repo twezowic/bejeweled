4f6093d56fe71d2f9426091efbbddc6ebbd124f3
twezowic <01169258@pw.edu.pl>
2022-01-07 02:14:22 +0100

    Added reset method and created Level class

--- game.py
+++ game.py
@@ -1,14 +1,17 @@
 from config import number_of_moves
+from leaderboard import Score
 
 
 class Game:
-    def __init__(self, score=0, moves=number_of_moves, normal_mode=True, name=None, highscore=0):
+    def __init__(
+            self,
+            score=Score(),
+            moves=number_of_moves,
+            normal_mode=True,
+            highscore=0):
         self._moves = moves
         self._score = score
         self._normal_mode = normal_mode
-        if name is None:
-            name = ''
-        self._name = name
         self._highscore = highscore
 
     def moves(self):
@@ -23,23 +26,25 @@ class Game:
     def score(self):
         return self._score
 
-    def set_score(self, new_score):
-        self._score = new_score
-
     def normal_mode(self):
         return self._normal_mode
 
     def change_normal_mode(self):
         self._normal_mode = not self.normal_mode()
 
-    def name(self):
-        return self._name
-
-    def set_name(self, new_name):
-        self._name = new_name
-
     def highscore(self):
         return self._highscore
 
     def set_highscore(self, new_highscore):
         self._highscore = new_highscore
+
+    def reset(self, board):
+        board.setup_board(self)
+        self.set_moves(number_of_moves)
+        self._score = Score()
+        self._normal_mode = True
+
+
+class Level:
+    def __init__(self) -> None:
+        pass
