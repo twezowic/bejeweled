2ba0c66ae35d0065083b65778925b3dec30e6e03
twezowic <01169258@pw.edu.pl>
2022-01-07 02:20:12 +0100

    Moved function.

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
 from colorsys import hls_to_rgb
 from math import floor
 
-board_width = 8  # min 8
-board_height = 8  # min 6
-SCREEN_WIDTH = board_width*50+110
-SCREEN_HEIGHT = board_height*50+10
-number_of_jewels = 6
-number_of_moves = 3
-
 
 def choose_colors(number_of_colors):
     colors = []
@@ -18,4 +11,10 @@ def choose_colors(number_of_colors):
     return colors
 
 
+board_width = 8  # min 8
+board_height = 8  # min 6
+SCREEN_WIDTH = board_width*50+110
+SCREEN_HEIGHT = board_height*50+10
+number_of_jewels = 6
+number_of_moves = 3
 colors_of_jewels = choose_colors(number_of_jewels)
