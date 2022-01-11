b54613f5e075aad6fa425c7fedba9ad7da511096
twezowic <01169258@pw.edu.pl>
2022-01-11 13:51:24 +0100

    Moved Board and Leaderboard class to Game .

--- game.py
+++ game.py
@@ -1,51 +1,52 @@
-from config import number_of_moves
-from leaderboard import Score
+from board import Board
+from config import number_of_moves, basic_goal
+from leaderboard import Leadeboard, Score
 
 
 class Level:
     def __init__(
             self,
-            normal_mode=True,
+            normal=True,
             which_level=1,
-            goal=800,
+            goal=basic_goal,
             moves=number_of_moves):
-        self._normal_mode = normal_mode
+        self._normal = normal
         self._level = which_level
         self._goal = goal
         self._moves = moves
 
-    def normal_mode(self):
-        return self._normal_mode
+    def normal(self):
+        return self._normal
 
-    def change_normal_mode(self):
-        self._normal_mode = not self.normal_mode()
+    def change_normal(self):
+        self._normal = not self.normal()
 
     def level(self):
         return self._level
 
-    def next_level(self, game, board):
+    def next_level(self, game):
         self._level += 1
-        self.add_to_goal(200)
-        self.set_moves(number_of_moves)
-        game.reset(board)
+        self.set_goal()
+        self.reset_moves()
+        game.reset()
 
     def goal(self):
         return self._goal
 
-    def add_to_goal(self, added_goal):
-        self._goal += added_goal
+    def set_goal(self):
+        self._goal = (self.level() - 1) * 200 + 800
 
     def moves(self):
         return self._moves
 
-    def set_moves(self, new_moves):
-        self._moves = new_moves
+    def reset_moves(self):
+        self._moves = number_of_moves
 
     def one_move(self):
-        self.set_moves(self.moves()-1)
+        self._moves -= 1
 
     def win_condition(self, score):
-        if not self.normal_mode():
+        if not self.normal():
             return False
         return score >= self.goal()
 
@@ -55,13 +56,20 @@ class Game:
             self,
             score=Score(),
             level=Level(),
-            highscore=0):
+            board=Board(),
+            leaderboard=Leadeboard()):
         self._score = score
         self._level = level
-        self._highscore = highscore
+        self._board = board
+        leaderboard.load('endless')
+        leaderboard.load('normal')
+        self._leaderboard = leaderboard
 
     def level(self):
         return self._level
+
+    def reset_level(self):
+        self._level = Level()
 
     def score(self):
         return self._score
@@ -69,12 +77,13 @@ class Game:
     def set_score(self, new_score):
         self._score = new_score
 
-    def highscore(self):
-        return self._highscore
+    def board(self):
+        return self._board
 
-    def set_highscore(self, new_highscore):
-        self._highscore = new_highscore
+    def leaderboard(self):
+        return self._leaderboard
 
-    def reset(self, board):
-        board.setup_board()
+    def reset(self):
+        self.board().setup_board()
         self.score().reset_score()
+        self.reset_level()
