a369f292b601a7b8b87c95a38df069ea219c026f
twezowic <01169258@pw.edu.pl>
2022-01-11 13:51:49 +0100

    Removed unnecessary classes.

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from interface import interface
-from board import Board
 from game import Game
-from leaderboard import Leadeboard
 
 
 def main():
-    board = Board()
     game = Game()
-    leaderboard = Leadeboard()
-    leaderboard.load()
-    game.set_highscore(leaderboard.get_highscore())
-    board.setup_board()
-    interface(board, game, leaderboard)
+    interface(game)
 
 
 if __name__ == '__main__':
