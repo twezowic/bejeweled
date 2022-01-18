9f937722f8503ffc9561aeb7a845fb15196872f8
twezowic <01169258@pw.edu.pl>
2022-01-18 10:01:21 +0100

    Added parser.

--- Bejeweled/main.py
+++ Bejeweled/main.py
@@ -1,12 +1,16 @@
 from interface import interface
 from game import Game
+from game_parser import create_parser
+import sys
 
 
-def main():
+def main(args):
+    arguments = create_parser(args)
+    print(arguments)
     game = Game()
-    game.setup()
+    game.leaderboard().setup()
     interface(game)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
