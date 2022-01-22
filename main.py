26ec46d58a8d70361b8e35bcce5138f651538900
twezowic <01169258@pw.edu.pl>
2022-01-22 20:58:20 +0100

    moved main file outside of folder

---
+++ main.py
@@ -0,0 +1,16 @@
+from bejeweled.interface import interface
+from bejeweled.game import Game
+from bejeweled.game_parser import create_parser
+import sys
+
+
+def main(args):
+    arguments = create_parser(args)
+    print(arguments)
+    game = Game(arguments)
+    game.leaderboard().load_from_file()
+    interface(game)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
