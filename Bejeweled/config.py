9f78ff5290c9c9b61db98c14f9e8e74d6c712ff1
twezowic <01169258@pw.edu.pl>
2022-01-22 19:33:47 +0100

    Deleted config file.

--- Bejeweled/config.py
+++
@@ -1,7 +0,0 @@
-board_width = 12  # min 8
-board_height = 12  # min 8
-SCREEN_WIDTH = board_width*50+110
-SCREEN_HEIGHT = board_height*50+10
-number_of_jewels = 6
-number_of_moves = 15
-basic_goal = 800
